37c8236bfbcac7ec489f1edfd44c38b36a13bae3
Murray Tait <death.tumble@gmail.com>
2018-06-23 21:44:57 +0100

    heirarchy of goss files

--- scripts/goss_setup.sh
+++ scripts/goss_setup.sh
@@ -2,12 +2,16 @@
 set -e
 
 sudo mkdir -p /etc/goss
-sudo mkdir -p /etc/config
 
 sudo mv /home/ec2-user/files/bin/goss /usr/local/bin/goss
 sudo chmod +x /usr/local/bin/goss
+sudo chown root:root /usr/local/bin/goss
 
 sudo mv /home/ec2-user/files/config/goss.yaml /etc/goss/goss.yaml 
+sudo chown root:root /etc/goss/goss.yaml
+
+sudo mv /home/ec2-user/files/config/goss-host.yaml /etc/goss/goss-host.yaml 
+sudo chown root:root /etc/goss/goss-host.yaml
 
 sudo mv /home/ec2-user/files/init.d/goss /etc/init.d/goss 
 sudo chmod +x /etc/init.d/goss
