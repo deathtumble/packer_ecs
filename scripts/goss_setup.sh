e0f3f641e336eb44377e3474275bcca598d867a6
Murray Tait <death.tumble@gmail.com>
2018-06-23 18:10:03 +0100

    Fixing startup of goss and modd

--- scripts/goss_setup.sh
+++ scripts/goss_setup.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /etc/config
 sudo mv /home/ec2-user/files/bin/goss /usr/local/bin/goss
 sudo chmod +x /usr/local/bin/goss
 
-sudo mv /home/ec2-user/files/config/goss.yaml /etc/config/goss.yaml 
+sudo mv /home/ec2-user/files/config/goss.yaml /etc/goss/goss.yaml 
 
 sudo mv /home/ec2-user/files/init.d/goss /etc/init.d/goss 
 sudo chmod +x /etc/init.d/goss
--- scripts/modd_setup.sh
+++ scripts/modd_setup.sh
@@ -2,12 +2,11 @@
 set -e
 
 sudo mkdir -p /etc/modd
-sudo mkdir -p /etc/config
 
 sudo mv /home/ec2-user/files/bin/modd /usr/local/bin/modd
 sudo chmod +x /usr/local/bin/modd
 
-sudo mv /home/ec2-user/files/config/modd.conf /etc/config/modd.conf 
+sudo mv /home/ec2-user/files/config/modd.conf /etc/modd/modd.conf 
 
 sudo mv /home/ec2-user/files/init.d/modd /etc/init.d/modd 
 sudo chmod +x /etc/init.d/modd
