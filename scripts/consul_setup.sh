ce6ffef2b143dec34baf1fbc14c594e83494179a
Murray Tait <death.tumble@gmail.com>
2018-08-18 19:42:50 +0100

    And the fix

--- scripts/consul_setup.sh
+++ scripts/consul_setup.sh
@@ -7,6 +7,6 @@ sudo mv /home/ec2-user/files/bin/consul /usr/local/bin/consul
 sudo chmod +x /usr/local/bin/consul
 sudo chown root:root /usr/local/bin/consul
 
-sudo mv /home/ec2-user/files/init.d/goss /etc/init.d/consul 
+sudo mv /home/ec2-user/files/init.d/consul /etc/init.d/consul 
 sudo chmod +x /etc/init.d/consul
 sudo chown root:root /etc/init.d/consul
