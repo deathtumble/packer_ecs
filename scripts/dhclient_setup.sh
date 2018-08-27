679972f165d0412483c93026b026bda1c67a7a27
Murray Tait <death.tumble@gmail.com>
2018-08-27 18:01:45 +0100

    Better command for amending dns server

--- scripts/dhclient_setup.sh
+++ scripts/dhclient_setup.sh
@@ -2,4 +2,4 @@
 
 set -e
   
-sudo echo "prepend domain-name-servers 127.0.0.1;" >> /etc/dhcp/dhclient.conf
+sudo sh -c 'printf "\nprepend domain-name-servers 127.0.0.1;\n" >> /etc/dhcp/dhclient.conf'
