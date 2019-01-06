4ab41741560ebfcad6b845e89e1c99ace598dab2
Murray Tait <death.tumble@gmail.com>
2019-01-06 18:40:53 +0000

    Fixing filebeat script for names and permissions

--- scripts/filebeats.sh
+++ scripts/filebeats.sh
@@ -3,11 +3,14 @@ sudo rpm -vi filebeat-6.5.4x86_64.rpm
 
 sudo mkdir -p /etc/filebeat/containers
 sudo chown root:root /etc/filebeat/containers/
+sudo chmod go-w /etc/filebeat/containers/
 
-sudo mv /home/ec2-user/files/config/filebeat.yml /etc/filebeat/filebeat.yaml 
-sudo chown root:root /etc/filebeat/filebeat.yaml
+sudo mv /home/ec2-user/files/config/filebeat.yml /etc/filebeat/filebeat.yml 
+sudo chown root:root /etc/filebeat/filebeat.yml
+sudo chmod go-w /etc/filebeat/filebeat.yml
 
-sudo mv /home/ec2-user/files/config/containers /etc/filebeat/containers 
+sudo mv /home/ec2-user/files/config/containers /etc/filebeat 
 sudo chown root:root /etc/filebeat/containers/*
+sudo chmod go-w /etc/filebeat/containers/*
 
 sudo service filebeat start
