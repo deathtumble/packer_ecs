ad447bbef3e2ddd66adc1d0327534113dc4c1d4c
Murray Tait <death.tumble@gmail.com>
2019-01-12 13:38:17 +0000

    Adding monit service, configuration initrc to start it and for monit to start filebeats

---
+++ scripts/monit_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+sudo yum install -y monit
+
+sudo mv /home/ec2-user/files/config/monit.conf /etc/monit.conf 
+sudo chown root:root /etc/monit.conf
+sudo chmod 600 /etc/monit.conf
+
+sudo sh -c 'echo "mo:2345:respawn:/usr/local/bin/monit -I" >> /etc/inittab'
