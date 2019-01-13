0ac6645e6ab31e5fb30912f7553a4168859d6bba
Murray Tait <death.tumble@gmail.com>
2019-01-13 00:30:39 +0000

    removing append to inittab

--- scripts/monit_setup.sh
+++ scripts/monit_setup.sh
@@ -6,5 +6,3 @@ sudo yum install -y monit
 sudo mv /home/ec2-user/files/config/monit.conf /etc/monit.conf 
 sudo chown root:root /etc/monit.conf
 sudo chmod 600 /etc/monit.conf
-
-sudo sh -c 'echo "mo:2345:respawn:/usr/bin/monit -I" >> /etc/inittab'
